516bd329a7b6c2861975d4f116e77bf282339eb5
Валентин <27902270+vdistortion@users.noreply.github.com>
2025-05-17 20:28:56 +0500

    clear

--- src/core/production.ts
+++ src/core/production.ts
@@ -1,23 +1,7 @@
-import createDebug from 'debug';
 import { bot } from '../index';
-
-const debug = createDebug('bot:production');
 
 const VERCEL_URL = process.env.VERCEL_URL;
 
 if (!VERCEL_URL) throw new Error('VERCEL_URL is not set.');
 
-debug('Bot runs in production mode');
-debug(`setting webhook: ${VERCEL_URL}`);
-
-bot
-  .init()
-  .then(() => bot.api.getWebhookInfo())
-  .then(async (getWebhookInfo) => {
-    if (getWebhookInfo.url !== `${VERCEL_URL}/api`) {
-      debug(`deleting webhook ${VERCEL_URL}`);
-      await bot.api.deleteWebhook();
-      debug(`setting webhook: ${VERCEL_URL}/api`);
-      await bot.api.setWebhook(`${VERCEL_URL}/api`);
-    }
-  });
+bot.api.setWebhook(`${VERCEL_URL}/api`).then(console.info).catch(console.error);
